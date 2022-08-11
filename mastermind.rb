23a555f843f12a355abe7e012b9ae0eccc18621a
Winslow Solomon <winsolo11@gmail.com>
2022-08-11 16:55:27 -0400

    Make message and writer
    In this commit I added a long message to be printed at the beginning of
    the game. I also added a method which will slowly print out a message
    one letter at a time as if it is being typed out. This method will be
    used to print out everything in the game. This commit also layed out
    the outline of how the rest of the code will work - now just a few more
    methods need to be made.

--- mastermind.rb
+++ mastermind.rb
@@ -8,20 +8,42 @@ class Game
   def initialize
     @guesses = 0
     generate_code
+    play
   end
 
   private
 
+  def play
+    message = "The year is 2054. You have hacked into a fiat wallet worth well over a whole bitcoin.\n"\
+              "There is only one remaining wall between you and the money. A 4 digit code. You have tried\n"\
+              "to bypass it, but it is impossible. You will have to guess. From what you can see\n"\
+              "in the code database for the wallet, it seems that the code has no repeats, and\n"\
+              "the program will tell you whether or not each digit in your guess is included in the code,\n"\
+              "and if it is, whether or not it is in the right spot. A small line of red text below\n"\
+              'the area you enter the code in spells out a message - YOU HAVE 3 TRIES LEFT - . Good luck.'
+    type_out(message)
+    3.times { guess }
+    win ? win_message : lose_message
+  end
+
+  def type_out(message)
+    message.each_char do |char|
+      print char
+      case char
+      when '.' then sleep(0.35)
+      when ',' then sleep(0.2)
+      else sleep(0.1)
+      end
+    end
+  end
+
   def generate_code
-    puts 'Generating code...'
-    sleep(1.5)
     @code = []
     4.times { add_num }
-    puts "The code is #{@code}"
   end
 
   def add_num
     num = (rand * 7).to_i.to_s
-    @code.include?(num) ? new_num : @code.push(num)
+    @code.include?(num) ? add_num : @code.push(num)
   end
 end
